0e6660335ed7d3e229b4a1bb9e18930b59296939
rparkavi55 <37235068+rparkavi55@users.noreply.github.com>
2018-03-10 14:44:09 +0530

    Add files via upload

---
+++ codekata/arith.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      RGCET
+#
+# Created:     10/03/2018
+# Copyright:   (c) RGCET 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    a=int(input())
+    if(a<=100)
+    print("positive")
+    else:
+        print("negative")
+
+if __name__ == '__main__':
+    main()
