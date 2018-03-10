5cc9e239af390a61a5bce1c2c86ed126320a1288
rparkavi55 <37235068+rparkavi55@users.noreply.github.com>
2018-03-10 15:22:36 +0530

    Add files via upload

---
+++ codekata/arithmetic.py
@@ -0,0 +1,29 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      RGCET
+#
+# Created:     10/03/2018
+# Copyright:   (c) RGCET 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+  a=20
+  b=40
+  print(a+b)
+  print(a-b)
+  print(a*b)
+  print(a/b)
+  print(a%b)
+  print(a**b)
+  print(a<>b)
+  print(a==b)
+  print(a<=b)
+  print(a>=b)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ codekata/positive.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      RGCET
+#
+# Created:     10/03/2018
+# Copyright:   (c) RGCET 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+  vars = int(input("enter a number:"))
+  if (1<=5<=500):
+    print("positive number")
+  elif 5==0:
+    print("zero")
+  else:
+     print("negative number")
+
+
+if __name__ == '__main__':
+    main()
