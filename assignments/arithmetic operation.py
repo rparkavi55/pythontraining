748db64cbac692eb5017a1972448470736e0a3be
rparkavi55 <37235068+rparkavi55@users.noreply.github.com>
2018-03-10 15:20:08 +0530

    Add files via upload

---
+++ assignments/arithmetic operation.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      RGCET
+#
+# Created:     10/03/2018
+# Copyright:   (c) RGCET 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+   a=20
+   b=40
+   print(a+b)
+   print(a*b)
+   print(a/b)
+
+if __name__ == '__main__':
+    main()
